33ac4e7e00c99f67f14669ee88258327feb9d04c
Karasawa47 <karasawa47@gmail.com>
2014-07-30 15:08:51 -0400

    added recipe view model
    RecipeIngredientDetailView

---
+++ GCGroupProject/viewModels/RecipeIngredientDetailView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCGroupProject.viewModels
+{
+    public class RecipeIngredientDetailView
+    {
+        public int RecipeID { get; set; }
+        public string RecipeTitle { get; set; }
+        public int PrepTime { get; set; }
+        public string Steps { get; set; }
+        public int CookTime { get; set; }
+        public ICollection<string> IngredientNames { get; set; }
+    }
+}
