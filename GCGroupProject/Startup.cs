2e7452f966c4fa9fc47cc7909ea1a8d77b46c3fd
William O'Sullivan <williamosullivan@gmail.com>
2014-07-30 09:41:59 -0400

    Initial Project Create
    Created

---
+++ GCGroupProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GCGroupProject.Startup))]
+namespace GCGroupProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
